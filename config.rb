2a7f1ab2b924d4e1d7803a61aa732a379cd4924b
Gerard Ketuma <me@gerardketuma.com>
2014-12-30 00:26:51 -0600

    setup new hpstr theme for my blog

--- config.rb
+++ config.rb
@@ -8,12 +8,13 @@ http_path = "/"
 http_images_path = "/images"
 http_generated_images_path = "/images"
 http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+#css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
 images_dir = "source/images"
 fonts_dir = "source/fonts"
+css_dir = "source/stylesheets"
 
 line_comments = false
 output_style = :compressed
